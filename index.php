6d1020778a46f703558f5a72bd2af5d50af81922
annamalai <annamalai@annamalai-desktop>
2013-07-27 16:58:56 +0530

    Signed-off-by: annamalai <annamalai@annamalai-desktop>

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?php
+<html>
+<head>
+<title>Sri Nachammal</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="css/style.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+	<header class="show-page-oferta" style="top: 0px;">
+
+
+		<nav>
+			<ul>
+				<li><a id="go-start" href="#"
+					class="goto ico0 menu0"> HOME </a></li>
+				<li><a id="go-o-nas" href="#"
+					class="goto ico1 menu1"> CURRICULUM </a></li>
+				<li><a id="go-oferta"
+					href="#"
+					class="goto ico2 menu2"> SCHOOL </a></li>
+				<li><a id="go-jak-pracujemy"
+					href="#"
+					class="goto ico3 menu3"> GALLERY </a></li>
+				<li><a id="go-cennik"
+					href="#"
+					class="goto ico4 menu4"> NEWS </a></li>
+				<li><a id="go-kontakt"
+					href="#"
+					class="goto ico5 menu5"> CONTACTUS </a></li>
+			</ul>
+		</nav>
+
+
+	</header>
+
+
+</body>
+
+</html>
