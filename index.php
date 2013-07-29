aa8756b132c8dc46f6549b2b3db4caeea19051ee
annamalai <annamalai@annamalai-desktop>
2013-07-29 12:55:11 +0530

    Signed-off-by: annamalai <annamalai@annamalai-desktop>

--- index.php
+++ index.php
@@ -7,34 +7,75 @@
 
 </head>
 <body>
-	<header class="show-page-oferta" style="top: 0px;">
+	<div class="sport">
+		<header class="show-page-oferta" style="top: 0px;">
 
 
-		<nav>
-			<ul>
-				<li><a id="go-start" href="#"
-					class="goto ico0 menu0"> HOME </a></li>
-				<li><a id="go-o-nas" href="#"
-					class="goto ico1 menu1"> CURRICULUM </a></li>
-				<li><a id="go-oferta"
-					href="#"
-					class="goto ico2 menu2"> SCHOOL </a></li>
-				<li><a id="go-jak-pracujemy"
-					href="#"
-					class="goto ico3 menu3"> GALLERY </a></li>
-				<li><a id="go-cennik"
-					href="#"
-					class="goto ico4 menu4"> NEWS </a></li>
-				<li><a id="go-kontakt"
-					href="#"
-					class="goto ico5 menu5"> CONTACTUS </a></li>
-			</ul>
-		</nav>
+			<nav>
+				<ul>
+					<li><a id="go-start" href="#" class="goto ico0 menu0"><b>HOME </b>
+					</a>
+					</li>
+					<li><a id="go-o-nas" href="#" class="goto ico1 menu1"> <b>CURRICULUM</b>
+					</a>
+					</li>
+					<li><a id="go-oferta" href="#" class="goto ico2 menu2"> <b>SCHOOL </b>
+					</a>
+					</li>
+					<li><a id="go-jak-pracujemy" href="#" class="goto ico3 menu3"><b>GALLERY</b>
+					</a>
+					</li>
+					<li><a id="go-cennik" href="#" class="goto ico4 menu4"> <b>NEWS</b>
+					</a>
+					</li>
+					<li><a id="go-kontakt" href="#" class="goto ico5 menu5"><b>
+								CONTACTUS </b>
+					</a>
+					</li>
+				</ul>
+			</nav>
 
 
-	</header>
+		</header>
 
 
+		<div class="circular">
+			<div class="cir-img">
+				<div class="circularimg">
+					<img alt="" src="img/child.png">
+				</div>
+			</div>
+		</div>
+
+		<div class="circular1">
+			<div class="cir-img1">
+				<div class="circularimg1">
+					<img alt="" src="img/child1.png">
+				</div>
+			</div>
+		</div>
+
+		
+		<div class="circular2">
+			<div class="cir-img2">
+				<div class="circularimg2">
+					<img alt="" src="img/text-img.png">
+				</div>
+			</div>
+		</div>
+		
+				
+		
+		<div class="circular3">
+			<div class="cir-img3">
+				<div class="circularimg3">
+					<img alt="" src="img/log1.png">
+				</div>
+			</div>
+		</div>
+
+
+	</div>
 </body>
 
 </html>
