7525511edb584d82455d68f802a2cca43fd94b64
annamalai <annamalai@annamalai-desktop>
2013-07-31 19:01:34 +0530

    Signed-off-by: annamalai <annamalai@annamalai-desktop>

--- index.php
+++ index.php
@@ -153,71 +153,8 @@
 		<img alt="" src="img/bottom.png">
 	</div>
 
-	<!-- footer -->
-	<div class="bot-footer col">
-		<div id="footer">
 
-			<div class="footbox">
-				<h1>Contact Us</h1>
-				<ul>
-					<li><b>SRI NACHAMMAL VIDYAVANI</b><br>Ammasai Nagar,Ayyavu Tower,Devarayampalayam,<br>
-					   Bye pass road(New 4 Track),,Avinashi, Tirupur (Dt).</li>
-					
-					
-					<li class="last">+ 91 4296 291744<br>+91 80568 12088, | +91 77080 07988<br>info@nachammal.com</li>
-				</ul>
-			</div>
-
-			<div class="footbox1">
-			<img  alt="" src="img/flogo.png">
-				<h2>Group of Companies</h2>
-				<img class="fline" alt="" src="img/fline.png">
-				<ul>
-					<li><b>The Chennai Silks</b></li>
-					<li><b>Sree Kumaran</b><br>Thangamaligai</li>
-					<li><b>Scm Group</b><br>companies</li>
-					
-					
-				</ul>
-				
-				<img class="fline1" alt="" src="img/fline.png">
-				
-				
-				<div class="f-child">
-				<img alt="" src="img/fchild.jpg">
-				
-				</div>
-			</div>
-
-
-
-
-
-		</div>
-
-
-	</div>
-
-	<!-- copywright -->
-	<div class="bot-footer1 col1">
-
-		<div id="copyright">
-		<div class="copy-1">
-			<p class="">
-				Copyright © Nachammal |Designed by<a href="#">S-Creative</a>
-			</p>
-			</div>
-			
-			<div class="copy-1">
-			<p class="">
-				Template by <a href="#" title="">OS Templates</a>
-			</p>
-			</div>
-			<br class="clear">
-		</div>
-	</div>
-
-
+	<?php include 'footer.php';?>
 
 
 
